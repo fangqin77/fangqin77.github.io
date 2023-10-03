ff45c559b1f8b45c07e85cb3d0dfa929c91aca74
fangqin77 <565294743@qq.com>
2023-10-03 10:55:01 +0800

    [resume] 添加简历模板

---
+++ resume/src/config.js
@@ -0,0 +1,127 @@
+export default {
+  // page1部分
+  page1: {
+    titleEn: 'Hello,I`m FangQin', //英文标题
+    title: '长路漫漫，唯剑作伴。', //中文标题
+  },
+  // page2部分
+  page2: {
+    authorImg: 'page1.jpg', // 作者头像
+    xinhui: '我叫方秦，是一名软件工程在读的大一学生。', // 幸会
+    qiuzhi: '不求职，读书呢', // 求职意向
+    guanyuwo: '且随疾风前行，身后亦须留心。' // 关于我
+  },
+  // page3部分
+  page3: [{
+    icon: 'icon-tubiao-',
+    title: 'UI设计',
+    msg: ['PS切图、界面排版']
+  },{
+    icon: 'icon-diannao', // 图标
+    title: '网页制作', // 标题
+    msg: ['响应式页面', 'css、scss动画效果',] //介绍
+  }, {
+    icon: 'icon-qianbi1',
+    title: '前端功能',
+    msg: ['JS完成常见的交互功能', '用AJAX读取后台数据且渲染']
+  },  {
+    icon: 'icon-shouji',
+    title: '框架',
+    msg: ['vue框架','UI框架、express框架、mysql',]
+  }],
+  // page4部分
+  page4: {
+    // 我的历程
+    course: [{
+      date: '2023/9——至今', // 时间
+      desc: { // 经历
+        title: '河北师范大学',
+        list: ['河北师范大学', '软件开发公司实习']
+      }
+    }, {
+      date: '2020/9——2023/9',
+      desc: {
+        title: '黄冈泾河中学',
+        list: ['高中']
+      }
+    }],
+    // 我的拓展技能掌握
+    singlelist: [{
+      title: 'bootsrap',
+      text: '了解'
+    }, {
+      title: 'Element-ui',
+      text: '掌握'
+    }, {
+      title: 'mint-ui',
+      text: '掌握'
+    }, {
+      title: 'scss',
+      text: '掌握'
+    }],
+    // 我的基本技能掌握
+    proresslist: [{
+      title: 'HTML、CSS、SCSS',
+      value: '90%'
+    }, {
+      title: 'JS、JQ',
+      value: '90%'
+    }, {
+      title: 'Vue',
+      value: '90%'
+    }, {
+      title: 'NodeJs',
+      value: '80%'
+    }, {
+      title: 'sql',
+      value: '80%'
+    }]
+  },
+  // page5部分
+  page5: [{
+    title: '项目1',
+    content: '项目1项目1项目1项目1项目1',
+    image: 'box1.png',
+    href: 'https://baidu.com/'
+  }, 
+  {
+    title: '项目2',
+    content: '项目1项目1项目1项目1项目1',
+    image: 'box1.png',
+    href: 'https://baidu.com/'
+  },
+  {
+    title: '项目3',
+    content: '项目1项目1项目1项目1项目1',
+    image: 'box1.png',
+    href: 'https://baidu.com/'
+  },
+  {
+    title: '项目4',
+    content: '项目1项目1项目1项目1项目1',
+    image: 'box1.png',
+    href: 'https://baidu.com/'
+  },
+  {
+    title: '项目5',
+    content: '项目1项目1项目1项目1项目1',
+    image: 'box1.png',
+    href: 'https://baidu.com/'
+  },
+  {
+    title: '项目6',
+    content: '项目1项目1项目1项目1项目1',
+    image: 'box1.png',
+    href: 'https://baidu.com/'
+  }
+],
+  // page6部分
+  page6: {
+    github: 'https://github.com/fangqin77',
+    bili: '',
+    email: 'Mailto:fangqinisme@gmail.com?Subject=邮箱标题&Body=邮箱内容！',
+    zhihu: '',
+    weixin: "<img style='width:120px' src='' alt='加载失败'>",
+    qq: "<img style='width:120px' src='' alt='加载失败'>"
+  }
+}
